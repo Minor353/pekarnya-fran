f029e80e3a0b051fcc9600541fdf5c33785638fa
Singularity112 <Singularity123097@gmail.com>
2018-11-09 17:38:06 +0200

    Завершена адаптация под мобайл.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,4 @@
+/**Аккордеон */
 $('.js-accordeon dd:not(:first)').slideUp();
 
 $('.js-accordeon dt').on('click', (ev) => {
@@ -8,6 +9,8 @@ $('.js-accordeon dt').on('click', (ev) => {
 
 
 
+
+/**Слайдер с отзывами */
 $('.reviews_slider-wrap').slick({
   centerMode: true,
   centerPadding: '200px',
@@ -18,12 +21,12 @@ $('.reviews_slider-wrap').slick({
   autoplaySpeed: 5000,
   responsive: [
     {
-      breakpoint: 768,
+      breakpoint: 850,
       settings: {
         arrows: false,
         centerMode: true,
         centerPadding: '40px',
-        slidesToShow: 3
+        slidesToShow: 1
       }
     },
     {
@@ -31,7 +34,7 @@ $('.reviews_slider-wrap').slick({
       settings: {
         arrows: false,
         centerMode: true,
-        centerPadding: '40px',
+        centerPadding: '0px',
         slidesToShow: 1
       }
     }
@@ -39,3 +42,32 @@ $('.reviews_slider-wrap').slick({
 });
 
 
+
+/**Мобильное меню**/
+$(document).ready(function() {
+  $('.menu-icon').click(function() {
+      $(this).toggleClass('active');
+      $('.header-mobile_bg').toggleClass('header-mobile_bg--active');
+      $('.heade-mobile').toggleClass('heade-mobile--active');
+  })
+})
+
+
+/********Форма обратной связи**********/
+
+const formWrap = document.querySelector('.form-wrapper');
+
+const btnForm = document.querySelectorAll('.form-link');
+
+for(let i = 0; i < btnForm.length; i++) {
+  btnForm[i].addEventListener('click', function(e){
+    e.preventDefault();
+    formWrap.classList.add('form-wrapper--active');
+  })
+
+  formWrap.addEventListener('click', function(){
+    if(event.target == formWrap) {
+      formWrap.classList.remove('form-wrapper--active');
+    }
+  })
+}
